85c0f1f5bc5e8a7ea9d07e8efda41ca56b2fdf94
John Patrick C. Perez <patrickperez527@gmail.com>
2022-03-16 17:49:23 +0800

    added bootstrap

--- script.js
+++ script.js
@@ -26,21 +26,31 @@ async function addRepos(username) {
 
 function createUserCard(user) {
     const card = `
-    <div class="card">
-        <img src="${user.avatar_url}" alt="${user.login}" class="avatar">
-        <div class="user-info">
-            <h2>${user.name}</h2>
-            <p>${user.bio}</p>
-            <ul>
-                <li>${user.followers} <strong>followers</strong></li>
-                <li>${user.following} <strong>following</strong></li>
-                <li>${user.public_repos} <strong>repos</strong></li>
-            </ul>
-            <div id="repos">
+        <div class="card mt-4 m-auto p-3 border bg-transparent" style="max-width: 650px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${user.avatar_url}" class="img-fluid rounded-start w-100 p-3" alt="${user.login}">
+                    <div class="mt-1 d-flex justify-content-center flex-wrap">
+                        <span class="mx-2 bg-danger text-white rounded mb-2 px-2">${user.followers} <strong>followers</strong></span>
+                        <span class="mx-2 bg-danger text-white rounded mb-2 px-2">${user.following} <strong>following</strong></span>
+                        <span class="mx-2 bg-danger text-white rounded mb-2 px-2">${user.public_repos} <strong>repos</strong></span>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">${user.name}</h5>
+                        <p class="mb-0">${user.bio}</p>
+                        <h5 class="mt-3 mb-2 card-title fw-bold">Repositories</h5>
+                        <div class="d-flex flex-wrap">
+                            <div id="repos">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
     `;
+
     main.innerHTML = card;
 }
 
@@ -51,7 +61,7 @@ function addReposToCard(repos) {
         repoEl.textContent = repo.name;
         repoEl.href = repo.html_url;
         repoEl.target = '_blank';
-        repoEl.classList.add('repos');
+        repoEl.classList.add('btn', 'btn-primary', 'rounded-pill', 'badge', 'm-1', 'text-decoration-underline', 'text-light');
 
         reposEl.appendChild(repoEl);
     })
@@ -59,7 +69,7 @@ function addReposToCard(repos) {
 
 function createErrorCard(msg) {
     const card = `
-    <div class="card">
+    <div class="card text-center p-5 border-2 mt-5 bg-danger text-white">
         <h1>${msg}</h1>
     </div>
     `;
